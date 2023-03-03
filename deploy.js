0213c119de4f88bbd853ccd47c2714c0354fa894
vesnatodor <vesnaatodorovic@gmail.com>
2023-03-03 09:42:43 +0100

    Komitovala sam

---
+++ deploy.js
@@ -0,0 +1,4 @@
+const URL = process.argv[2],
+   location = process.argv[3];
+console.log(URL);
+console.log(location);
---
+++ test.js
@@ -0,0 +1,6 @@
+// console.log('Preko node a');
+// console.log(1+3);
+const x = process.argv[2];
+const poruka =`Ono što sam otkucao je: ${x}`;
+console.log(typeof x);
+console.log(poruka);
